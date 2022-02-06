ab3302cf11d9ff0ae1b6f560f7bf7465467f53bb
Ayesha1806 <96306552+Ayesha1806@users.noreply.github.com>
2022-02-06 17:41:30 +0530

    Add files via upload

---
+++ find_grad_girls.java
@@ -0,0 +1,20 @@
+package Assignment2;
+
+public class find_grad_girls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub,
+		float student=90f;
+		float b=40f;
+		float girls;
+		girls=student-b;
+		float grade;
+		float n1=20f;
+		grade=(90/2);
+		float n2=grade-n1;
+		System.out.println("A grils syuents are : " +n2);
+		
+
+	}
+
+}
